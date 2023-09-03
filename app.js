87849939056ed1f4d672d122791a26a312f3ac54
developerkhairul <126383915+developerkhairul@users.noreply.github.com>
2023-09-03 17:50:11 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(e) {
+    $('.btn').on('mouseenter', function(e) {
+        x = e.pageX - $(this).offset().left;
+        y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({
+            top: y,
+            left: x
+        });
+    });
+    $('.btn').on('mouseout', function(e) {
+        x = e.pageX - $(this).offset().left;
+        y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({
+            top: y,
+            left: x
+        });
+    });
+});
